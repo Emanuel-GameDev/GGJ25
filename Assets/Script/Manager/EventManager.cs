87b504d8a84a6b9cab976a00c451cc6a9cd0fc87
Edoardo Del Sarto <e99delsarto@gmail.com>
2025-01-23 23:48:22 +0100

    Update: + PlayerStats + PlayerLevelManager + EventManager

---
+++ Assets/Script/Manager/EventManager.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public static class EventManager
+{
+    public static UnityAction OnPlayerDeath;
+    public static UnityAction<int> OnPlayerLevelUp;
+}
