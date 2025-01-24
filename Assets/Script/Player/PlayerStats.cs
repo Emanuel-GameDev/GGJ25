323a84ac9bddd36cf346136ba0142825a4d5b578
Edoardo Del Sarto <e99delsarto@gmail.com>
2025-01-24 14:50:35 +0100

    Update: + Regain oxygen player
    Clone:
    + Code cleaning

--- Assets/Script/Player/PlayerStats.cs
+++ Assets/Script/Player/PlayerStats.cs
@@ -9,6 +9,7 @@ public class PlayerStats : MonoBehaviour, IDamageable
     [SerializeField] private float _maxOxygen = 100f;
     [SerializeField] private bool _carryBubble = false;
     [SerializeField] private float _oxygenLossRate = 1f;
+    [SerializeField] private float _oxygenGainRate = 2f;
     [SerializeField] private float _healthLossRate = 1f;
 
     [SerializeField] private float _invincibilityTime = 2f;
@@ -27,11 +28,18 @@ public class PlayerStats : MonoBehaviour, IDamageable
         {
             _oxygen -= _oxygenLossRate * Time.deltaTime;
         }
+        else if(_carryBubble 
+                && _oxygen < _maxOxygen)
+        {
+            _oxygen += _oxygenGainRate * Time.deltaTime;
+        }
 
         if(_oxygen <= 0)
         {
             _health -= _healthLossRate * Time.deltaTime;
         }
+
+        
     }
 
     public void SetCarryBubble(bool isCarringBubble)
@@ -70,10 +78,10 @@ public class PlayerStats : MonoBehaviour, IDamageable
 
     private void SetBubbleCarring(GameObject player)
     {
-        Debug.Log("Player: " + player.name);
+        // Debug.Log("Player: " + player.name);
         if(player == gameObject)
         {
-            Debug.Log("TRUE Player: " + player.name);
+            // Debug.Log("TRUE Player: " + player.name);
             if(_carryBubble)
                 _carryBubble = false;
             else
